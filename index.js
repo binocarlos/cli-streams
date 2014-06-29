b4212a0d57eca346bed2c4f1d15f45ce7a918282
Kai Davenport <kaiyadavenport@gmail.com>
2014-06-29 13:44:57 +0000

    test for the file streams

--- index.js
+++ index.js
@@ -1,8 +1,42 @@
 var path = require('path')
+var fs = require('fs')
 
-module.exports = function(filepath){
-  if(filepath.match(/^\w+:/) || filepath.match(/^\//)){
-    return filepath
-  }
-  return path.resolve(process.cwd(), filepath)
+module.exports = function(filepaths){
+
+	filepaths = filepaths || {}
+
+	var inputStream = process.stdin
+	var outputStream = process.stdout
+
+	if(filepaths.input){
+		if(typeof(filepaths.input)==='string'){
+			if(!fs.existsSync(filepaths.input)){
+		    console.error('error - file: ' + inputpath + ' does not exit')
+		    process.exit(1)
+		  }
+		  inputStream = fs.createReadStream(filepaths.input)
+		}
+		else{
+			inputStream = filepaths.input
+		}
+	}
+
+	if(filepaths.output){
+		if(typeof(filepaths.output)==='string'){
+		  var outputfolder = path.dirname(filepaths.output)
+		  if(!fs.existsSync(outputfolder)){
+		    console.error('error - folder: ' + outputfolder + ' does not exit')
+		    process.exit(1)
+		  }
+		  outputStream = fs.createWriteStream(filepaths.output)
+		}
+		else{
+			outputStream = filepaths.output
+		}
+	}
+
+	return {
+		input:inputStream,
+		output:outputStream
+	}
 }
--- test.js
+++ test.js
@@ -1,9 +1,22 @@
 var tape = require('tape')
-var streams = require('./')
+var clistreams = require('./')
+var fs = require('fs')
 
 tape('test a filepipe', function(t){
-	var resolved = resolve('package.json')
 
-	t.equal(resolved, __dirname + '/package.json')
-	t.end()
+	var streams = clistreams({
+		input:__dirname + '/package.json',
+		output:__dirname + '/package.out.json'
+	})
+
+	streams.output.on('close', function(){
+		var packageorig = require(__dirname + '/package.json')
+		var packageout = require(__dirname + '/package.out.json')
+
+		t.deepEqual(packageorig, packageout, 'the files are the same')
+		fs.unlinkSync(__dirname + '/package.out.json')
+		t.end()
+	})
+
+	streams.input.pipe(streams.output)
 })
